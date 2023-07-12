46849b8850e504759f0dece59eaf2f56bffa16e3
Евгений <@LAPTOP-IEGUVAC3>
2023-07-12 16:59:10 +0700

    Templates_DONE

--- Arrays_not_overload/Source.cpp
+++ Arrays_not_overload/Source.cpp
@@ -1,234 +1,87 @@
 #include<iostream>
 using namespace std;
-#define tab "\t"
-#define delimiter "\n----------------------------------\n"
-const int ROWS = 3;
-const int COLS = 4;
 
-void FillRand(int arr[], const int n);
-void FillRand(double arr[], const int n);
-void FillRand(int arr[ROWS][COLS], const int ROWS, const int COLS);
-
-void Print(const int arr[], const int n);
-void Print(const double arr[], const int n);
-void Print(const int arr[ROWS][COLS], const int ROWS, const int COLS);
-
-void Sort(int arr[], const int n);
-void Sort(double arr[], const int n);
-
-int  Sum(const int arr[], const int n);
-double Sum(const double arr[], const int n);
-int Sum(const int arr[ROWS][COLS], const int ROWS, const int COLS);
-
-double Avg(const int arr[], const int n);
-double Avg(const double arr[], const int n);
-double Avg(const int arr[ROWS][COLS], const int ROWS, const int COLS);
-
-int minValueIn(const int arr[], const int n);
-double minValueIn(const double arr[], const int n);
-
-int maxValueIn(const int arr[], const int n);
-double maxValueIn(const double arr[], const int n);
+template <class A> void FillRand(A arr[], const int n);
+template <class A> void Print(A arr[], const int n);
+template <class A> void Sort(A arr[], const int n);
+template <class A> A Sum(A arr[], const int n);
+template <class A> A Average(A arr[], const int n);
+template <class A> A Min(A arr[], const int n);
+template <class A> A Max(A  arr[], const int n);
 
 void main()
 {
 	setlocale(LC_ALL, "");
 	const int n = 5;
 	int arr[n];
-
 	FillRand(arr, n);
-	Print(arr, n);
-	cout << "Сумма элементов массива: " << Sum(arr, n) << endl;
-	cout << "Среднее-арифметическое элементов массива: " << Avg(arr, n) << endl;
-	cout << "Минимальное значение в массиве: " << minValueIn(arr, n) << endl;
-	cout << "Максимальное значение в массиве: " << maxValueIn(arr, n) << endl;
+	Print(arr, n); cout << endl;
 	Sort(arr, n);
-	Print(arr, n);
+	Print(arr, n); cout << endl;
+	cout << "Сумма элементов массива: " << Sum(arr, n) << endl;
+	cout << "Среднее арифметическое : " << Average(arr, n) << endl;
+	cout << "Минимальное значение в массиве : " << Min(arr, n) << endl;
+	cout << "Максимальное значение в массиве : " << Max(arr, n) << endl;
 
-	const int SIZE = 8;
-	double brr[SIZE];
-
-	FillRand(brr, SIZE);
-	Print(brr, SIZE);
-	cout << "Сумма элементов массива: " << Sum(brr, SIZE) << endl;
-	cout << "Среднее-арифметическое элементов массива: " << Avg(brr, SIZE) << endl;
-	cout << "Минимальное значение в массиве: " << minValueIn(brr, SIZE) << endl;
-	cout << "Максимальное значение в массиве: " << maxValueIn(brr, SIZE) << endl;
-	Sort(brr, SIZE);
-	Print(brr, SIZE);
-
-	int i_arr_2[ROWS][COLS];
-	FillRand(i_arr_2, ROWS, COLS);
-	Print(i_arr_2, ROWS, COLS);
-	cout << "Сумма элементов массива:\t" << Sum(i_arr_2, ROWS, COLS) << endl;
-	cout << "Среднее-арифметическое элементов массива:\t" << Avg(i_arr_2, ROWS, COLS) << endl;
 }
-
-void FillRand(int arr[], const int n)
+template <class A> void FillRand(A arr[], const int n)
 {
-	//Заполнение массива случайными числами:
 	for (int i = 0; i < n; i++)
 	{
 		arr[i] = rand() % 100;
 	}
 }
-void FillRand(double arr[], const int n)
+template <class A> void Print(A arr[], const int n)
 {
-	//Заполнение массива случайными числами:
 	for (int i = 0; i < n; i++)
 	{
-		arr[i] = rand() % 10000;
-		arr[i] /= 100;
+		cout << arr[i] << "\t";
 	}
 }
-void FillRand(int arr[ROWS][COLS], const int ROWS, const int COLS)
+template <class A> void Sort(A arr[], const int n)
 {
-	for (int i = 0; i < ROWS; i++)
+	for (int j = 0; j < n; j++)
 	{
-		for (int j = 0; j < COLS; j++)
+		for (int i = 0; i < n - 1; i++)
 		{
-			arr[i][j] = rand() % 100;
-		}
-	}
-}
-void Print(const int arr[], const int n)
-{
-	//Вывод массива на экран:
-	for (int i = 0; i < n; i++)
-	{
-		cout << arr[i] << tab;
-	}
-	cout << endl;
-}
-void Print(const double arr[], const int n)
-{
-	//Вывод массива на экран:
-	for (int i = 0; i < n; i++)
-	{
-		cout << arr[i] << tab;
-	}
-	cout << endl;
-}
-void Print(const int arr[ROWS][COLS], const int ROWS, const int COLS)
-{
-	for (int i = 0; i < ROWS; i++)
-	{
-		for (int j = 0; j < COLS; j++)
-		{
-			cout << arr[i][j] << tab;
-		}
-		cout << endl;
-	}
-}
-void Sort(int arr[], const int n)
-{
-	//Сортировка массива:
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = i + 1; j < n; j++)
-		{
-			if (arr[j] < arr[i])
+			if (arr[i] > arr[i + 1])
 			{
-				int buffer = arr[i];
-				arr[i] = arr[j];
-				arr[j] = buffer;
+				A buf = arr[i];
+				arr[i] = arr[i + 1];
+				arr[i + 1] = buf;
 			}
 		}
 	}
 }
-void Sort(double arr[], const int n)
+template <class A> A Sum(A arr[], const int n)
 {
-	//Сортировка массива:
+	A summ = 0;
 	for (int i = 0; i < n; i++)
 	{
-		for (int j = i + 1; j < n; j++)
-		{
-			if (arr[j] < arr[i])
-			{
-				double buffer = arr[i];
-				arr[i] = arr[j];
-				arr[j] = buffer;
-			}
-		}
+		summ += arr[i];
 	}
+	return(A)summ;
 }
-
-int  Sum(const int arr[], const int n)
+template <class A> A Average(A arr[], const int n)
 {
-	int sum = 0;
+	A avg = Sum(arr, n) / n;
+	return (A)avg;
+}
+template <class A> A Min(A arr[], const int n)
+{
+	A min = arr[0];
 	for (int i = 0; i < n; i++)
 	{
-		sum += arr[i];
-	}
-	return sum;
-}
-double Sum(const double arr[], const int n)
-{
-	double sum = 0;
-	for (int i = 0; i < n; i++)
-	{
-		sum += arr[i];
-	}
-	return sum;
-}
-int Sum(const int arr[ROWS][COLS], const int ROWS, const int COLS)
-{
-	int sum = 0;
-	for (int i = 0; i < ROWS; i++)
-	{
-		for (int j = 0; j < COLS; j++)
-		{
-			sum += arr[i][j];
-		}
-	}
-	return sum;
-}
-double Avg(const int arr[], const int n)
-{
-	return (double)Sum(arr, n) / n;
-}
-double Avg(const double arr[], const int n)
-{
-	return (double)Sum(arr, n) / n;
-}
-double Avg(const int arr[ROWS][COLS], const int ROWS, const int COLS)
-{
-	return (double)Sum(arr, ROWS, COLS) / (ROWS * COLS);
-}
-
-int minValueIn(const int arr[], const int n)
-{
-	int min = arr[0];
-	for (int i = 0; i < n; i++)
-	{
-		if (arr[i] < min)min = arr[i];
+		if (arr[i] < min) min = arr[i];
 	}
 	return min;
 }
-double minValueIn(const double arr[], const int n)
+template <class A> A Max(A arr[], const int n)
 {
-	double min = arr[0];
+	A max = arr[0];
 	for (int i = 0; i < n; i++)
 	{
-		if (arr[i] < min)min = arr[i];
-	}
-	return min;
-}
-int maxValueIn(const int arr[], const int n)
-{
-	int max = arr[0];
-	for (int i = 0; i < n; i++)
-	{
-		if (arr[i] > max)max = arr[i];
-	}
-	return max;
-}
-double maxValueIn(const double arr[], const int n)
-{
-	double max = arr[0];
-	for (int i = 0; i < n; i++)
-	{
-		if (arr[i] > max)max = arr[i];
+		if (arr[i] > max) max = arr[i];
 	}
 	return max;
 }
